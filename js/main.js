10ec614b91381dceaa037f3d4c46f89db73f35eb
victor <victorvicentini@yahoo.com.br>
2020-05-22 10:01:07 -0300

    Aula 2

---
+++ js/main.js
@@ -0,0 +1,28 @@
+/*var nome = "Victor Vicentini"
+var idade="33"
+//alert("Meu nome é "+nome+" e tenho "+idade+"anos.")
+console.log(nome)
+console.log(idade)
+console.log(n1)
+
+//alert("Meu Primeiro JS")
+
+var frase="Japão é o melhor time do mundo"
+
+console.log(frase.replace("Japão","Brasil"))
+console.log(frase.toLowerCase())
+console.log(frase.toUpperCase())*/
+
+var lista = ["maça", "pera", "laranja"];
+lista.push("uva")
+lista.pop()
+console.log(lista);
+console.log(lista.length);
+console.log(lista.reverse());
+console.log(lista.toString())
+console.log(lista.join(" - "))
+
+
+var fruta={nome: "maça", cor:"vermelha"}
+console.log(fruta.nome)
+console.log(fruta.cor)
